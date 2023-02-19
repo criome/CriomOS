fa93f168a16479cbe79fc3e825a38ed09d9cd5cd
li <@li:maisiliym.kriom>
2023-02-19 13:32:18 +0700

    (fix (ffmpeg withMfx))

--- nix/homeModule/min/sway.nix
+++ nix/homeModule/min/sway.nix
@@ -13,7 +13,7 @@ let
   waylandQtpass = pkgs.qtpass.override { pass = waylandPass; };
   waylandPass = pkgs.pass.override { x11Support = false; waylandSupport = true; };
 
-  hardwareAdjustedFfmpeg = pkgs.ffmpeg.override { libmfxSupport = hasQuickSyncSupport; };
+  hardwareAdjustedFfmpeg = pkgs.ffmpeg.override { withMfx = hasQuickSyncSupport; };
 
   shellLaunch = command: "${shell} -c '${command}'";
   homeDir = config.home.homeDirectory;
