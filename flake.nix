ffc5ef8887d75df5f2b7043f848f3f374b0ae655
li <@li:maisiliym.kriom>
2023-06-21 20:19:20 +0700

    (switchedBranch hob purvaPhalguniFun)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "KriomOS - KriOS on Linux";
 
   inputs = {
-    hob.url = github:sajban/hob;
+    hob.url = github:sajban/hob/purvaPhalguniFun;
     mkWebpage = { url = path:./mkWebpage; flake = false; };
     kor = { url = path:./nix/kor; flake = false; };
     mkPkgs = { url = path:./nix/mkPkgs; flake = false; };
